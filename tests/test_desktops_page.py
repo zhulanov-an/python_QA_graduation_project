2c90645787b7da3a519fd2d66b79a4d72d5a22f1
Anton Zhulanov <zhulanoff2007@gmail.com>
2021-05-31 17:56:08 +0300

    добавил title для тестов категорий продуктов

--- tests/test_desktops_page.py
+++ tests/test_desktops_page.py
@@ -1,3 +1,4 @@
+import allure
 import pytest
 from pages.CategoryPage import CategoryPage
 
@@ -10,21 +11,25 @@ def desktops_page(browser, base_url):
     return page
 
 
+@allure.title('тест проверки заголовка браузера')
 def test_browser_title_is_desktops(desktops_page):
     """проверка заголовка страницы Desktops"""
     assert desktops_page.title == "Desktops"
 
 
+@allure.title('тест проверки заголовка в странице')
 def test_text_of_header_of_category_page(desktops_page):
     """проверка наличия заголовка Desktops в странице"""
     assert desktops_page.header_category.text == "Desktops"
 
 
+@allure.title('тест отображения описания блока')
 def test_text_description(desktops_page):
     """проверка отображения описания блока"""
     assert desktops_page.description_category.text == "Example of category description text"
 
 
+@allure.title('тест количества блоков в категории')
 def test_count_elements_in_category(desktops_page):
     """проверка количества блоков в разделе Desktops"""
     items = desktops_page.items_in_category
